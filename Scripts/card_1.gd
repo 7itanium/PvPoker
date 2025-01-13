2c89870ae57b1666221970557aa3b397e2ce2541
Thomas <tph0905@gmail.com>
2025-01-13 14:18:27 -0600

    Dealing cards

---
+++ Scripts/card_1.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+var target_position = Vector2(-400, 275)  # Target position
+var speed = 5.0  # Adjust this to control the movement speed
+
+@onready var game_manager: Node = %"Game Manager"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(0, 0)  # Start at (0, 0).
+	print(game_manager.p1_Cards)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta: float) -> void:
+	if game_manager.p1_Cards >= 1:
+		position.x = lerp(position.x, target_position.x, speed * delta)
+		position.y = lerp(position.y, target_position.y, speed * delta)
+
+		# Optional: Snap to the target position when close enough
+		if position.distance_to(target_position) < 1:
+			position = target_position
---
+++ Scripts/game_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var p1_Cards = 0
+var p2_Cards = 0
+
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("P1-Hit"):
+		p1_Cards += 1
+		print("Player 1 has " + str(p1_Cards) + " cards")
+		
+	if Input.is_action_just_pressed("P2-Hit"):
+		p2_Cards += 1
+		print("Player 2 has " + str(p2_Cards) + " cards")
+	if Input.is_action_just_pressed("Reset"):
+		get_tree().reload_current_scene()

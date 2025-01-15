2d5a5785666f98aacbfcfde8629e68ce679dc1c3
Thomas <tph0905@gmail.com>
2025-01-15 13:06:04 -0600

    Randomizing card drawing

--- Scripts/2card_1.gd
+++ Scripts/2card_1.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 var target_position = Vector2(400, -275)  # Target position
 var speed = 5.0  # Adjust this to control the movement speed
+var flipped = 0
 
 @onready var game_manager: Node = %"Game Manager"
 
@@ -9,6 +10,9 @@ var speed = 5.0  # Adjust this to control the movement speed
 func _ready() -> void:
 	position = Vector2(0, 0)  # Start at (0, 0).
 	print(game_manager.p2_Cards)
+	
+	var dynamic_path = "res://Sprites/Cards/back.png"
+	texture = load(dynamic_path)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,8 +22,11 @@ func _process(delta: float) -> void:
 		position.x = lerp(position.x, target_position.x, speed * delta)
 		position.y = lerp(position.y, target_position.y, speed * delta)
 
-		var dynamic_path = "res://Sprites/Cards/Diamonds/eight.png"
-		texture = load(dynamic_path)
+		if flipped == 0:
+			var card = game_manager.deck[game_manager.cards[randi() % game_manager.cards.size()]]
+			var dynamic_path = card[1]
+			texture = load(dynamic_path)
+			flipped = 1
 
 		# Optional: Snap to the target position when close enough
 		if position.distance_to(target_position) < 1:
